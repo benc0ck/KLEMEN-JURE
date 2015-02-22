c2206e4f218e58be7af8f23d29b8eefe9395a242
benc0ck <jurebenkovic@yahoo.com>
2015-02-22 15:11:23 +0100

    Game Added

---
+++ Game/Assets/Level 1/Scripts/BGLooperScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGLooperScript : MonoBehaviour {
+
+	int numBGPanels = 3;
+
+	float pipeMax = 2.63f;
+	float pipeMin = -1f;
+
+
+	void Start() {
+	GameObject[] monsters = GameObject.FindGameObjectsWithTag("Monsters");
+
+	foreach(GameObject monster in monsters) {
+		Vector3 pos = monster.transform.position;
+		pos.y = Random.Range (pipeMin, pipeMax);
+		monster.transform.position = pos;
+
+	}
+}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		Debug.Log ("Triggered: " + collider.name);
+
+		float widthOfBGObject = ((BoxCollider2D)collider).size.x;
+
+		Vector3 pos = collider.transform.position;
+
+		pos.x += widthOfBGObject * numBGPanels;
+
+		if (collider.tag == "Monster") {
+			pos.y = Random.Range (pipeMin, pipeMax);
+		}
+
+		collider.transform.position = pos;
+
+
+	}
+}
---
+++ Game/Assets/Level 1/Scripts/NikoMovement.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NikoMovement : MonoBehaviour {
+
+		Vector3 velocity = Vector3.zero;
+		public float fireSpeed = 100f;
+		public float forwardSpeed = 1f;
+		
+		bool didFlap = false;
+
+		Animator animator;
+
+		bool dead = false;
+		float deathCooldown;
+
+		// Use this for initialization
+		void Start () {
+		animator = transform.GetComponentInChildren<Animator> ();
+
+
+
+		if (animator == null) {
+						Debug.LogError ("Didn't find animator!");
+				}
+			
+		}
+		
+		// Do Graphich & Inpud updates here
+		void Update() {
+				if (dead) {
+						deathCooldown -= Time.deltaTime;
+
+						if (deathCooldown <= 0) {
+								if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) {
+										Application.LoadLevel (Application.loadedLevel);
+								}
+						}
+				}
+		else {
+			if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) {
+				didFlap = true;
+						}
+			
+				}
+		}
+		
+				
+		// Do physics engine updates here
+		void FixedUpdate () {
+
+		if(dead)
+			return;
+
+		rigidbody2D.AddForce (Vector2.right * forwardSpeed);
+
+		if(didFlap) {
+			rigidbody2D.AddForce (Vector2.up * fireSpeed);
+			animator.SetTrigger("DoFire");
+
+
+			didFlap = false;
+				
+
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		animator.SetTrigger("Death");
+		dead = true;
+		deathCooldown = 0.5f;
+
+		if (dead) {
+
+				}
+	}
+}
+
+	
+	
+	
+
+	
+	
+	
+
